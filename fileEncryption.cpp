1e239f850b13ee527b85e2673af297574615df75
Jonathan9719 <jonathanmax97@gmail.com>
2024-04-15 13:03:03 -0400

    file encryption

---
+++ fileEncryption.cpp
@@ -0,0 +1,70 @@
+#include <iostream>
+#include <fstream>
+#include <string>
+
+using namespace std;
+
+void encryptFile (const string &inputFilePath, const string outputFilePath, int key)
+{
+    ifstream inputFile(inputFilePath, ios::binary);
+    ofstream outputFile(outputFilePath, ios::binary);
+
+    char ch;
+    while (inputFile.get(ch))
+    {
+        outputFile.put(ch + key);
+    }
+
+    inputFile.close();
+    outputFile.close();
+}
+
+void decryptFile(const string &inputFilePath, const string outputFilePath, int key)
+{
+    ifstream inputFile(inputFilePath, ios::binary);
+    ofstream outputFile(outputFilePath, ios::binary);
+
+    char ch;
+    while (inputFile.get(ch))
+    {
+        outputFile.put(ch - key);
+    }
+
+    inputFile.close();
+    outputFile.close();
+}
+
+int main()
+{
+    string inputFilePath, outputFilePath;
+    int choice, key;
+
+    cout << "1. Encrypt File\n"
+        << "2. Decrypt File\n"
+        << "Enter your choice: ";
+    cin >> choice;
+
+    cout << "Enter key (integer): ";
+    cin >> key;
+
+    cout << "Enter input file path: ";
+    cin >> inputFilePath;
+    cout << "Enter output file path: ";
+    cin >> outputFilePath;
+
+    if (choice == 1)
+    {
+        encryptFile(inputFilePath, outputFilePath, key);
+    }
+    else if (choice == 2)
+    {
+        decryptFile(inputFilePath, outputFilePath, key);
+    }
+    else
+    {
+        cout << "Invalid choice.\n";
+    }
+
+
+    return 0;
+}
